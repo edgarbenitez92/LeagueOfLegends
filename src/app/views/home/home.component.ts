048dd76c45e9cbb62bb5ad5eeef16a445f119c0c
Edgar Benitez <edgar.benitez@derivatik.com>
2022-06-16 14:21:46 -0400

    feat(home): navbar selector
    navbar selector has been incorporated in the homeComponent

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  opened: boolean = true;
+  opened: boolean = false;
   menu: string[] = [];
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 }
