9c1c1c04bdb7086e4514642b2b89760153a74c60
Edgar Benitez <edgar.benitez@derivatik.com>
2022-06-17 14:21:32 -0400

    feat(sharedmodule): tooltip
    MatTooltip incorporated  with customs configurations

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -5,19 +5,16 @@ import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
   @Input() sidenav!: MatSidenav;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   openAppSettingsDialog() {
     // this.dialog.open(AppSettingsDialogComponent);
   }
-
 }
