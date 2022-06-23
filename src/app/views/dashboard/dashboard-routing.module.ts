bb1facacb85b27e15d1b6d16e0c3aed813bd61ce
Edgar Benitez <edgar.benitez@derivatik.com>
2022-06-23 14:07:09 -0400

    feat(dashboard): miniIconChampions routing
    miniIconsChampions path created to dashboard routing

--- src/app/views/dashboard/dashboard-routing.module.ts
+++ src/app/views/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { MiniIconsChampionsComponent } from './mini-icons-champions/mini-icons-champions.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard/home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      { path: 'home', component: MiniIconsChampionsComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { MiniIconsChampionsComponent } from './mini-icons-champions/mini-icons-champions.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, MiniIconsChampionsComponent],
   imports: [
     SharedModule,
     DashboardRoutingModule
