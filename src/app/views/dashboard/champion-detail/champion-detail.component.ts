aff711ba4de338a5eea14267266825c186a89925
Edgar Benitez <edgarbenitez92@gmail.com>
2022-06-24 10:57:46 -0400

    feat(championdetail): championDetail Component
    ChampionDetail component, routing and scss has been created

---
+++ src/app/views/dashboard/champion-detail/champion-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-champion-detail',
+  templateUrl: './champion-detail.component.html',
+  styleUrls: ['./champion-detail.component.scss']
+})
+export class ChampionDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { ChampionsListComponent } from './champions-list/champions-list.component';
+import { ChampionDetailComponent } from './champion-detail/champion-detail.component';
 
 @NgModule({
-  declarations: [DashboardComponent, ChampionsListComponent],
+  declarations: [DashboardComponent, ChampionsListComponent, ChampionDetailComponent],
   imports: [SharedModule, DashboardRoutingModule],
 })
 export class DashboardModule {}
