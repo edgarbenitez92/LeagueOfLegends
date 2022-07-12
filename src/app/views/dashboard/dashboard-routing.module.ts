c253f975f9be8322469afcedd03be93752a7d1ae
Edgar Benitez <edgar.benitez@derivatik.com>
2022-07-12 17:38:06 -0400

    feat(app-routing): changes in app-routing
    Changes in differents paths in all routing modules

--- src/app/views/dashboard/dashboard-routing.module.ts
+++ src/app/views/dashboard/dashboard-routing.module.ts
@@ -24,4 +24,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class DashboardRoutingModule {}
+export class DashboardRoutingModule { }
