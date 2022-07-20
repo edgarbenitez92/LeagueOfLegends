8308a93cac2c8b5df46beffc8504f0ab7fde4ab8
Edgar Benitez <edgarbenitez92@gmail.com>
2022-07-20 13:29:41 -0400

    feat(aboutme): aboutMe incorporation
    About component created and incorporated. General styles, navigations and more...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
