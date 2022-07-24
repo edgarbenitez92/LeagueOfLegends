bbf9d25bcdae073018ed2414fabd10b7275fd52f
Edgar Benitez <edgarbenitez92@gmail.com>
2022-07-24 19:43:52 -0400

    feat(coremodule): moduleImport created
    The guard ModuleImport has been created. This guard can protect the CoreModule to avoid unnecesary
    imports on other modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { throwIfAlreadyLoaded } from './guards/module-import.guard';
 
 @NgModule({
   declarations: [],
   imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
 })
-export class CoreModule {}
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+}
