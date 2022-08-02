28f543250fb0a5e4abb8a553944ad53bb82c43e5
Edgar Benitez <edgarbenitez92@gmail.com>
2022-08-02 18:07:09 -0400

    feat(pwa): pWA implementation
    Package Angular PWA installed. Settings and code need it to implements the PWA and auto updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'leagueOfLegends';
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit(): void {
+    this.updatePWA();
+  }
+
+  updatePWA() {
+    this.swUpdate.activateUpdate().then((value) => {
+      window.location.reload();
+    });
+  }
 }
