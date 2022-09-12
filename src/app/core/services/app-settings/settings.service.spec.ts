9091b7111df1938f7b882f46d7c740177c371f26
Edgar Benitez <edgarbenitez92@gmail.com>
2022-09-12 12:36:35 -0400

    feat(appsettingsservice): unit test
    Implementation of unit tests

--- src/app/core/services/app-settings/settings.service.spec.ts
+++ src/app/core/services/app-settings/settings.service.spec.ts
@@ -2,6 +2,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppSettingsService } from './app-settings.service';
 import { TestBed } from '@angular/core/testing';
 import { TranslateService } from '@ngx-translate/core';
+import { environment } from 'src/environments/environment';
+import { AppSettings } from '../../../shared/interfaces/app-settings.interface';
+import { mockSettings } from './mock/lang-settings.mock';
 
 fdescribe('AppSettingsService', () => {
   let appSettingsService: AppSettingsService;
@@ -16,5 +19,29 @@ fdescribe('AppSettingsService', () => {
 
   it('it should be created', () => {
     expect(appSettingsService).toBeTruthy();
+  });
+
+  it('It should call the init function', () => {
+    const spy = spyOn(appSettingsService, 'init');
+    appSettingsService.init();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('It should to be same appSettings', () => {
+    spyOn(appSettingsService, 'init');
+    appSettingsService.init();
+    expect(mockSettings).toEqual(appSettingsService.appSettings);
+  });
+
+  it('It should call the getAppSettings function', () => {
+    const spy = spyOn(appSettingsService, 'getAppSettings');
+    appSettingsService.getAppSettings();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('It should call the toggleLanguage function', () => {
+    const spy = spyOn(appSettingsService, 'toggleLanguage');
+    appSettingsService.toggleLanguage('en');
+    expect(spy).toHaveBeenCalled();
   });
 });
