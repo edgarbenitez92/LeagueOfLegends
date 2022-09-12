94d4011b84feab9ce79c093a9acc206846ebd2c8
Edgar Benitez <edgarbenitez92@gmail.com>
2022-09-12 11:58:54 -0400

    feat(splash champion): unit test
    incorporation of unit tests

--- src/app/shared/components/splash-champion/splash-champion.component.spec.ts
+++ src/app/shared/components/splash-champion/splash-champion.component.spec.ts
@@ -1,24 +1,48 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { mockAmumu } from 'src/app/views/dashboard/champion-detail/mock/champion-detail.mock';
+import { Champion } from '../../interfaces/champions.interface';
 import { SplashChampionComponent } from './splash-champion.component';
 
-describe('SplashChampionComponent', () => {
+fdescribe('SplashChampionComponent', () => {
   let component: SplashChampionComponent;
   let fixture: ComponentFixture<SplashChampionComponent>;
+  let championCard: Champion;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SplashChampionComponent],
+      imports: [RouterTestingModule],
+      // providers: [
+      //   { provide: Router, useClass: FakeRouter }
+      // ]
     }).compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(SplashChampionComponent);
     component = fixture.componentInstance;
+
+    championCard = mockAmumu;
+    component.champion = championCard;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('it should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('it should show the champion name', () => {
+    const debugElement: DebugElement = fixture.debugElement;
+    const championNameLabel: HTMLElement = debugElement.query(By.css('h5')).nativeElement;
+    expect(championNameLabel.innerHTML).toContain('Amumu');
+  });
+
+  it('it should contain the route dashboard/champion/Amumu', () => {
+    const router: Router = TestBed.inject(Router);
+    const spy = spyOn(router, 'navigate');
+    component.goToChampionDetails(component.champion.id);
+    expect(spy).toHaveBeenCalledWith(['dashboard/champion/Amumu'])
   });
 });
