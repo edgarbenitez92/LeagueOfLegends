c0225765b5f52b24e8e55c8e008f1a53e7087c1a
Edgar Benitez <edgarbenitez92@gmail.com>
2023-06-27 19:39:31 -0400

    (Refactor) Api Version
    -. Se refactoriza la obtención de la versión de la API de LOL siendo que ahora no se llama en todo momennto
    -. Se creó una variable de session que almacenará este valor y por ahí los endpoints serán consumidos

--- src/app/core/services/champions/champions.service.ts
+++ src/app/core/services/champions/champions.service.ts
@@ -2,42 +2,51 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Data } from '../../../shared/interfaces/champions.interface';
-import { switchMap } from 'rxjs/operators';
+import { map, pluck } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { SummonerVersionService } from '../summoner-version/summoner-version.service';
 import { AppSettingsService } from '../app-settings/app-settings.service';
+import { SessionService } from '../session/session.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChampionsService {
+  apiVersion: string | null;
+
   constructor(
     private http: HttpClient,
-    private summonerService: SummonerVersionService,
-    private appSettings: AppSettingsService
-  ) {}
+    private appSettings: AppSettingsService,
+    private sessionService: SessionService
+  ) {
+    this.apiVersion = this.sessionService.getApiVersion();
+  }
 
   getChampions(): Observable<any> {
-    return this.summonerService
-      .getLeagueOfLegendsVersions()
+    return this.http
+      .get<any>(`${environment.apiBaseUrl}/${this.apiVersion}/data/en_US/champion.json`)
       .pipe(
-        switchMap((versions: string[]) =>
-          this.http.get<any>(`${environment.apiBaseUrl}/${versions[0]}/data/en_US/champion.json`)
-        )
+        pluck('data'),
+        map((data) => {
+          let championsCards: any[] = [];
+          let championsArr: any = Object.values(data);
+          for (let champion of championsArr) {
+            const name = champion.name;
+            const image = `${environment.apiBaseUrl}/img/champion/loading/${champion.id}_0.jpg`;
+            const id = champion.id;
+            const difficulty = champion.info.difficulty;
+            const roles = champion.tags;
+            const miniImage = `${environment.apiBaseUrl}/${champion.version}/img/champion/${champion.id}.png`;
+            championsCards.push({ name, image, miniImage, id, difficulty, roles });
+          }
+          return championsCards;
+        })
       );
   }
 
   getChampionById(id: string): Observable<Data> {
     let language = this.appSettings.appSettings.api_language;
-
-    return this.summonerService
-      .getLeagueOfLegendsVersions()
-      .pipe(
-        switchMap((versions: string[]) =>
-          this.http.get<Data>(
-            `${environment.apiBaseUrl}/${versions[0]}/data/${language}/champion/${id}.json`
-          )
-        )
-      );
+    return this.http.get<Data>(
+      `${environment.apiBaseUrl}/${this.apiVersion}/data/${language}/champion/${id}.json`
+    );
   }
 }
